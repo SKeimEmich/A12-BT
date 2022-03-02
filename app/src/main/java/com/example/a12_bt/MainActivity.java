70f3f1f55c4638ebeaf384a932e2e07f5af4d1f2
Giantte <gchambe2@emich.edu>
2022-03-02 10:58:22 -0500

    Version 2

--- app/src/main/java/com/example/a12_bt/MainActivity.java
+++ app/src/main/java/com/example/a12_bt/MainActivity.java
@@ -4,6 +4,10 @@ package com.example.a12_bt;
 import static com.example.a12_bt.R.*;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import java.util.UUID;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -16,11 +20,20 @@ import android.content.Context;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * Authors Giantte, Jean, Sam
  */
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    // BT Variables
+    private final String CV_ROBOTNAME = "EV3A";
+    private BluetoothAdapter cv_btInterface = null;
+    private Set<BluetoothDevice> cv_pairedDevices = null;
+    private BluetoothDevice cv_btDevice = null;
+    private BluetoothSocket cv_btSocket = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +76,13 @@ public class MainActivity extends AppCompatActivity {
         switch (menuItem.getItemId()) {
             case id.firstMenuOption:
                 cpf_requestBTPermissions();
+                return true;
+            case id.secondMenuOption:
+                cv_btDevice = cpf_locateInPairedBTList(CV_ROBOTNAME);
+                return true;
+            case id.thirdMenuOption:
+                cpf_connectToEV3(cv_btDevice);
+                return true;
         }
         binding.vvTvOut1.setText(menuItem.getTitle() + " Selected");
         return true;
@@ -110,6 +130,42 @@ public class MainActivity extends AppCompatActivity {
         else {
             Toast.makeText(MainActivity.this,
                     "BLUETOOTH_CONNECT already granted", Toast.LENGTH_SHORT) .show();
+        }
+    }
+
+    // Modify from chap14, pp390 findRobot()
+    private BluetoothDevice cpf_locateInPairedBTList(String name) {
+        BluetoothDevice lv_bd = null;
+        try {
+            cv_btInterface = BluetoothAdapter.getDefaultAdapter();
+            cv_pairedDevices = cv_btInterface.getBondedDevices();
+            Iterator<BluetoothDevice> lv_it = cv_pairedDevices.iterator();
+            while (lv_it.hasNext())  {
+                lv_bd = lv_it.next();
+                if (lv_bd.getName().equalsIgnoreCase(name)) {
+                    binding.vvTvOut1.setText(name + " is in paired list");
+                    return lv_bd;
+                }
+            }
+            binding.vvTvOut1.setText(name + " is NOT in paired list");
+        }
+        catch (Exception e) {
+            binding.vvTvOut1.setText("Failed in findRobot() " + e.getMessage());
+        }
+        return null;
+    }
+
+    // Modify frmo chap14, pp391 connectToRobot()
+    private void cpf_connectToEV3(BluetoothDevice bd) {
+        try  {
+            cv_btSocket = bd.createRfcommSocketToServiceRecord
+                    (UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+            cv_btSocket.connect();
+            binding.vvTvOut2.setText("Connect to " + bd.getName() + " at " + bd.getAddress());
+        }
+        catch (Exception e) {
+            binding.vvTvOut2.setText("Error interacting with remote device [" +
+                    e.getMessage() + "]");
         }
     }
 
