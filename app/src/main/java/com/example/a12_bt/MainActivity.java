684eacc4fc7d614d20a374ce4561faa4a7e811e1
Sam Keim <sblasko1@emich.edu>
2022-03-01 19:32:04 -0500

    Ver 01 Complete

--- app/src/main/java/com/example/a12_bt/MainActivity.java
+++ app/src/main/java/com/example/a12_bt/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.a12_bt;
 
 import static com.example.a12_bt.R.*;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.a12_bt.databinding.ActivityMainBinding;
@@ -24,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        // Need grant permission once per install
+        cpf_checkBTPermissions();
         binding.floatingActionButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -37,9 +43,10 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view){
-                        binding.textView.setText(string.COSC426Greeting);
+                        binding.vvTvOut1.setText(string.COSC426Greeting);
                     }
                 });
+
     }
 
     // Overriding onCreateoptionMenu() to make Option menu
@@ -53,7 +60,57 @@ public class MainActivity extends AppCompatActivity {
     //Overriding onOptionsItemSelected to perform event on menu items
     @Override
     public boolean onOptionsItemSelected(MenuItem menuItem) {
-        binding.textView.setText(menuItem.getTitle() + " Selected");
+        switch (menuItem.getItemId()) {
+            case id.firstMenuOption:
+                cpf_requestBTPermissions();
+        }
+        binding.vvTvOut1.setText(menuItem.getTitle() + " Selected");
         return true;
     }
+    private void cpf_checkBTPermissions() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_GRANTED) {
+            binding.vvTvOut1.setText("BLUETOOTH_SCAN already granted.\n");
+        }
+        else {
+            binding.vvTvOut1.setText("BLUETOOTH_SCAN NOT granted.\n");
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_DENIED) {
+            binding.vvTvOut2.setText("BLUETOOTH_CONNECT NOT granted.\n");
+        }
+        else {
+            binding.vvTvOut2.setText("BLUETOOTH_CONNECT already granted.\n");
+        }
+    }
+
+    // https://www.geeksforgeeks.org/android-how-to-request-permissions-in-android-application/
+    private void cpf_requestBTPermissions() {
+        // We can give any value but unique for each permission.
+        final int BLUETOOTH_SCAN_CODE = 100;
+        final int BLUETOOTH_CONNECT_CODE = 101;
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.BLUETOOTH_SCAN},
+                    BLUETOOTH_SCAN_CODE);
+        }
+        else {
+            Toast.makeText(MainActivity.this,
+                    "BLUETOOTH_SCAN already granted", Toast.LENGTH_SHORT) .show();
+        }
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.BLUETOOTH_CONNECT},
+                    BLUETOOTH_CONNECT_CODE);
+        }
+        else {
+            Toast.makeText(MainActivity.this,
+                    "BLUETOOTH_CONNECT already granted", Toast.LENGTH_SHORT) .show();
+        }
+    }
+
 }
